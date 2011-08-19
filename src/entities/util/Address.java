de3ae1b278cad095a88999478fd87378f500d8ef
tarcisocomp <tarcisocomp@6fb6ba25-01e0-d165-b681-2b0abd4214a7>
2011-08-19 02:15:09 +0000

    Fixing svn errors in code.

--- src/entities/util/Address.java
+++ src/entities/util/Address.java
@@ -1,74 +1,73 @@
 package entities.util;
 
 public class Address {
-	
-	private String street;
-	private String number;
-	private String neighborhood;
-	private String city;
-	private String state;
-	private String country;
-	private String zipCode;
-	
-	public Address(String street, String number, String neighborhood,
-			String city, String state, String country, String zipCode) {
-		this.street = street;
-		this.number = number;
-		this.neighborhood = neighborhood;
-		this.city = city;
-		this.state = state;
-		this.country = country;
-		this.zipCode = zipCode;
-	}
+        
+        private String street;
+        private String number;
+        private String neighborhood;
+        private String city;
+        private String state;
+        private String country;
+        private String zipCode;
+        
+        public Address(String street, String number, String neighborhood,
+                        String city, String state, String country, String zipCode) {
+                this.street = street;
+                this.number = number;
+                this.neighborhood = neighborhood;
+                this.city = city;
+                this.state = state;
+                this.country = country;
+                this.zipCode = zipCode;
+        }
 
-	public String getStreet() {
-		return this.street;
-	}
+        public String getStreet() {
+                return this.street;
+        }
 
-	public String getNumber() {
-		return this.number;
-	}
+        public String getNumber() {
+                return this.number;
+        }
 
-	public String getNeighborhood() {
-		return this.neighborhood;
-	}
+        public String getNeighborhood() {
+                return this.neighborhood;
+        }
 
-	public String getCity() {
-		return this.city;
-	}
+        public String getCity() {
+                return this.city;
+        }
 
-	public String getState() {
-		return this.state;
-	}
+        public String getState() {
+                return this.state;
+        }
 
-	public String getCountry() {
-		return this.country;
-	}
+        public String getCountry() {
+                return this.country;
+        }
 
-	public String getZipCode() {
-		return this.zipCode;
-	}
-	
-	@Override
-	public String toString(){
-		StringBuilder addressToString = new StringBuilder();
-		addressToString.append(this.street);
-		addressToString.append(this.number);
-		addressToString.append(this.neighborhood);
-		addressToString.append(this.city);
-		addressToString.append(this.state);
-		addressToString.append(this.country);
-		addressToString.append(this.zipCode);
-		return addressToString.toString();
-	}
+        public String getZipCode() {
+                return this.zipCode;
+        }
+        
+        @Override
+        public String toString(){
+                StringBuilder addressToString = new StringBuilder();
+                addressToString.append(this.street);
+                addressToString.append(this.number);
+                addressToString.append(this.neighborhood);
+                addressToString.append(this.city);
+                addressToString.append(this.state);
+                addressToString.append(this.country);
+                addressToString.append(this.zipCode);
+                return addressToString.toString();
+        }
 
-	@Override
-	public int hashCode(){
-		return this.toString().hashCode();
-	}
+        @Override
+        public int hashCode(){
+                return this.toString().hashCode();
+        }
 
-	public boolean addressMatches(String toBeMatched) {
-		return this.toString().toUpperCase().contains(toBeMatched.toUpperCase());
-	}
-	
+        public boolean addressMatches(String toBeMatched) {
+                return this.toString().toUpperCase().contains(toBeMatched.toUpperCase());
+        }
 }
--- src/main/LendMe.java
+++ src/main/LendMe.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import entities.User;
+import entities.util.Address;
 
 public class LendMe {
 

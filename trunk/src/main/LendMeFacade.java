d255c64952e41195a16bc3d88c1973622200cf38
guisgb13@gmail.com <guisgb13@gmail.com@6fb6ba25-01e0-d165-b681-2b0abd4214a7>
2011-09-04 21:33:37 +0000

    Added some info to Session and create an EventDate object that encapsulates a Date object and also info regarding the event that happened on that date. Also created necessary methods to acceptance test 01;

--- trunk/src/main/LendMeFacade.java
+++ trunk/src/main/LendMeFacade.java
@@ -4,19 +4,22 @@ import entities.User;
 
 public class LendMeFacade {
 	
+	public void zerarSistema(){
+		LendMe.resetSystem();
+	}
+	
 	public void criarUsuario(String login, String nome, String endereco) throws Exception {
 		LendMe.registerUser(login, nome, endereco);
 	}
 	
 	public String getAtributoUsuario(String login, String atributo) throws Exception{
 		
-		
 		if(atributo == null || atributo.trim().isEmpty()){
-			throw new Exception("Invalid attribute");
+			throw new Exception("Atributo inválido");//"Invalid attribute");
 		}
 		
 		if(!(atributo.equals("nome") || atributo.equals("endereco"))){
-			throw new Exception("Inexistent attribute");
+			throw new Exception("Atributo inexistente");//"Inexistent attribute");
 		}
 		
 		User user = LendMe.getUserByLogin(login);
@@ -26,6 +29,8 @@ public class LendMeFacade {
 		}else{
 			return user.getAddress().toString();
 		}
-	} 
+	}
+	
+	
 
 }

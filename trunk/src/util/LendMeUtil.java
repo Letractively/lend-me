a8c90d01ba534e0ac83ea932fefc75fc6faaefb6
tarcisocomp@gmail.com <tarcisocomp@gmail.com@6fb6ba25-01e0-d165-b681-2b0abd4214a7>
2011-09-11 19:53:48 +0000

    The LendMeUtil class was created for putting methods that are not necessarily responsability of the class who uses or has the objects.

---
+++ trunk/src/util/LendMeUtil.java
@@ -0,0 +1,43 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import entities.util.Message;
+import entities.util.Topic;
+
+public class LendMeUtil {
+	
+	public static String toOrganizedTopicsArray(List<Topic> topics) {
+		StringBuilder orgTopicsArray = new StringBuilder();
+		
+		for (int i = 0; i < topics.size(); i++) {
+			if (i != topics.size() - 1) {
+				orgTopicsArray.append(topics.get(i).getSubject() + "; ");
+			}
+			else {
+				orgTopicsArray.append(topics.get(i).getSubject());
+			}
+		}
+		return orgTopicsArray.toString();
+		
+	}
+	
+	public static String toOrganizedMessagesArray(List<Message> messages) {
+		StringBuilder orgMessagesArray = new StringBuilder();
+		
+		List<Message> msgList = new ArrayList<Message>(messages);
+		
+		for (int i = 0; i < msgList.size(); i++) {
+			if (i != msgList.size() - 1) {
+				orgMessagesArray.append(msgList.get(i).getMessage() + "; ");
+			}
+			else {
+				orgMessagesArray.append(msgList.get(i).getMessage());
+			}
+		}
+		return orgMessagesArray.toString();
+		
+	}
+}

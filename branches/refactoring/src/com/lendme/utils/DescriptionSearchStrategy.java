1440d8ff3dc425992eb02d8a565ae6ad38af69a5
guisgb13@gmail.com <guisgb13@gmail.com@6fb6ba25-01e0-d165-b681-2b0abd4214a7>
2011-10-26 14:04:16 +0000

    Now all search strategies can return the own searcher as a result. This behavior does NOT bring errors to acceptance test!

--- branches/refactoring/src/com/lendme/utils/DescriptionSearchStrategy.java
+++ branches/refactoring/src/com/lendme/utils/DescriptionSearchStrategy.java
@@ -2,6 +2,7 @@ package com.lendme.utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import com.lendme.entities.Item;
 import com.lendme.entities.User;
@@ -19,7 +20,9 @@ public class DescriptionSearchStrategy implements OrderedSearch {
 	public List<Item> doSearch(User searcher, String value) {
 
 		List<Item> result = new ArrayList<Item>();
-		for ( User friend : searcher.getFriends() ){
+		Set<User> scope = searcher.getFriends();
+		scope.add(searcher);
+		for ( User friend : scope ){
 			for ( Item item : friend.getAllItems() ){
 				if ( item.getDescription().toUpperCase().contains(value.toUpperCase()) ){
 					result.add(item);

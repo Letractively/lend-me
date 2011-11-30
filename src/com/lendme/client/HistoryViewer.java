e5ba22f4d958ba39400dd86731a0a60178c556bf
tarcisocomp <tarcisocomp@gmail.com>
2011-11-30 05:21:59 +0000

    Up to date History Viewer

--- src/com/lendme/client/HistoryViewer.java
+++ src/com/lendme/client/HistoryViewer.java
@@ -3,12 +3,16 @@ package com.lendme.client;
 import java.util.ArrayList;
 import java.util.Map;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -18,37 +22,26 @@ public class HistoryViewer extends Composite {
 	private VerticalPanel container = new VerticalPanel();
 	private final Label errorLabel; 
 	private final String errorMessage = "Erro: LendMe nao conseguiu obter informacao do servidor.";
-	private Label systemMsg;
-	private Label titulo;
-	private ScrollPanel scrollPanel;
-	private HorizontalPanel topPanel;
-	private Label numberOfMsgsLabel;
-	private Label subjectLabel;
+	final String REFRESH_ICON_PATH = "http://icons.iconarchive.com/icons/deleket/button/48/Button-Refresh-icon.png";
+	final String GROUP_ICON_PATH = "http://www.iconeasy.com/icon/thumbnails/Internet%20%26%20Web/Users/User%20group%20Icon.jpg";
+	final String PERSONAL_ICON_PATH = "http://icons.iconarchive.com/icons/custom-icon-design/pretty-office/48/personal-information-icon.png";
+	private String scope;
 	private LendMeAsync lendMeService;
 	private String solicitorSessionId;
-	private Image refreshIcon;
-	private Label mark1;
-	private Label mark2;
-	private DockPanel datePanel;
-	final String REFRESH_ICON_PATH = "http://icons.iconarchive.com/icons/deleket/button/48/Button-Refresh-icon.png";
-	final String GROUP_ICON_PATH = "http://icons.iconarchive.com/icons/fasticon/leopard-iphone/48/Group-Folder-icon.png";
-	final String PERSONAL_ICON_PATH = "http://icons.iconarchive.com/icons/fasticon/leopard-iphone/48/Users-Folder-icon.png";
-	private String scope;
-	private Label errorLabel;
-	private Image group;
-	private Image personalIcon;
-	private ArrayList<LendMeActivityRep> historyList;
-	private AbsolutePanel activityPanel;
-	
+	private VerticalPanel activityPanel; 
 	
 	public HistoryViewer(LendMeAsync lendMeService,String solicitorSessionId, String scope) {
+		
+		this.scope = scope;
+		this.solicitorSessionId = solicitorSessionId;
+		this.lendMeService = lendMeService;
 		
 		container.setStyleName("topicsLines");
 		
 		container.setWidth("570px");
 
 		ScrollPanel scrollPanel = new ScrollPanel();
-		rootPanel.add(scrollPanel, 0, 32);
+		rootPanel.add(scrollPanel, 10, 80);
 		scrollPanel.setSize("600px", "515px");
 		rootPanel.setSize("600px", "550px");
 		
@@ -74,103 +67,61 @@ public class HistoryViewer extends Composite {
 		
 		topPanel.addStyleName("topicsHeader");
 		
-		activityPanel = new AbsolutePanel();
-		activityPanel.setSize("700px", "562px");
-		activityPanel.setStyleName("backGround2");
-		final VerticalPanel activityPanel = new VerticalPanel();
-		container.add(activityPanel);
-
-		
-		systemMsg = new Label("");
-		systemMsg.addStyleName("barra");
-		systemMsg.setSize("700px", "10px");
-		rootPanel.add(systemMsg, 0, 58);
-		
-		titulo = new Label("Histórico de atividades");
-		titulo.addStyleName("defaultTitle");
-		titulo.setSize("260px", "29px");
-		rootPanel.add(titulo, 60, 19);
-		
-		mark1 = new Label("**");
-		rootPanel.add(mark1, 416, 23);
-		
-		mark2 = new Label("**");
-		mark2.setSize("14px", "20px");
-		rootPanel.add(mark2, 489, 23);
-
-		
-		rootPanel.add(scrollPanel, 0, 70);
-
-		
-
-		refreshIcon = new Image(REFRESH_ICON_PATH);
-		refreshIcon.addClickHandler(new ClickHandler() {
+		PushButton refreshButton = new PushButton("Refresh button");
+		refreshButton.getUpFace().setImage(new Image(REFRESH_ICON_PATH));
+		rootPanel.add(refreshButton, 427, 8);
+		refreshButton.setSize("48px", "48px");
+		refreshButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				refreshPage();
+				refresh();
 			}
 		});
-		refreshIcon.setSize("48px", "48px");
-		rootPanel.add(refreshIcon, 436, 4);
 		
-		refreshPage();
-
+		PushButton groupButton = new PushButton("Group button");
+		groupButton .getUpFace().setImage(new Image(GROUP_ICON_PATH));
+		rootPanel.add(groupButton , 341, 8);
+		groupButton .setSize("48px", "48px");
+		groupButton .addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				refresh("all");
+			}
+		});
 		
-		errorLabel = new Label("Ocorreu um erro inesperado! ");
-		errorLabel.setStyleName("defaultErrorStyle");
+		PushButton personalButton = new PushButton("Group button");
+		personalButton .getUpFace().setImage(new Image(PERSONAL_ICON_PATH));
+		rootPanel.add(personalButton , 513, 8);
+		personalButton .setSize("48px", "48px");
+		personalButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				refresh("personal");
+			}
+		});
+		
+		activityPanel = new VerticalPanel();
+		container.add(activityPanel);
+		activityPanel.setWidth("599px");
 		errorLabel.setVisible(false);
 		
-		rootPanel.add(scrollPanel, 0, 32);
+		rootPanel.add(scrollPanel, 10, 80);
 		
+		refresh();
 		
-		personalIcon = new Image(this.PERSONAL_ICON_PATH);
-		personalIcon.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				refreshPage("personalHistoric");
-			}
-		});
-		personalIcon.setSize("48px", "48px");
-		rootPanel.add(personalIcon, 517, 4);
-
 		this.initWidget(rootPanel);
 		
-		Image image = new Image((String) null);
-		image.setUrl("http://icons.iconarchive.com/icons/kawsone/teneo/48/2000Facts-2-icon.png");
-		rootPanel.add(image, 5, 5);
-		image.setSize("48px", "48px");
 	}
 	
-	private void refreshPage(){
-		refreshPage(this.scope);
+	private void refresh() {
+		refresh(this.scope);
 	}
 	
-	public void addTopicstoPanel() {
-		Iterator<LendMeActivityRep> myActivitiesIterator = historyList.iterator();
-		int i = 0;
-		while(myActivitiesIterator.hasNext()){
-			int top = i*70;
-			activityPanel.add(myActivitiesIterator.next(),top,0);
-			activityPanel.setHeight(Integer.toString((top + 70 ))+"px");
-			i++;
-		}
-		rootPanel.add(activityPanel);
-
-	}
-	
-	private void refreshPage(String scope){
+	private void refresh(String newScope) {
+		this.scope = newScope;
 		if ( scope.equals("all") ){
 			lendMeService.getJointActivityHistory(solicitorSessionId, new AsyncCallback<Map<String, ArrayList<String[]>>>() {
 
 				@Override
 				public void onSuccess(Map<String, ArrayList<String[]>> result) {
-					for (String date : result.keySet() ){
-						DockPanel datePanel = new DockPanel();
-						datePanel.setWidth("660px");
-						datePanel.add(new Label("Atividates em "+date), DockPanel.CENTER);
-						activityPanel.add(datePanel);
-						for ( String[] content : result.get(date) ){
-							activityPanel.add(new LendMeActivityRep(content[0], content[1], content[2]));
-						}
-					}
+					addActivitiesToPanel(result);
 				}
 
 				@Override
@@ -185,15 +136,7 @@ public class HistoryViewer extends Composite {
 
 				@Override
 				public void onSuccess(Map<String, ArrayList<String[]>> result) {
-					for (String date : result.keySet() ){
-						DockPanel datePanel = new DockPanel();
-						datePanel.setWidth("660px");
-						datePanel.add(new Label("Atividates em "+date), DockPanel.CENTER);
-						activityPanel.add(datePanel);
-						for ( String[] content : result.get(date) ){
-							activityPanel.add(new LendMeActivityRep(content[0], content[1], content[2]));
-						}
-					}
+					addActivitiesToPanel(result);
 				}
 
 				@Override
@@ -203,9 +146,18 @@ public class HistoryViewer extends Composite {
 				}
 			});
 		}
-		
-		this.initWidget(rootPanel);
-		
 	}
 	
+	private void addActivitiesToPanel(
+			Map<String, ArrayList<String[]>> result) {
+		for (String date : result.keySet() ){
+			DockPanel datePanel = new DockPanel();
+			datePanel.setWidth("660px");
+			datePanel.add(new Label("Atividates em "+date), DockPanel.CENTER);
+			activityPanel.add(datePanel);
+			for ( String[] content : result.get(date) ){
+				activityPanel.add(new LendMeActivityRep(content[0], content[1], content[2]));
+			}
+		}
+	}
 }

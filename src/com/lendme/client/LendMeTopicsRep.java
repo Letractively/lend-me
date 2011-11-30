fb4c9044712677435abc9579cc3c21341734088e
guisgb13 <guisgb13@gmail.com>
2011-11-30 18:50:39 +0000

    Fixes in the last commit.

--- src/com/lendme/client/LendMeTopicsRep.java
+++ src/com/lendme/client/LendMeTopicsRep.java
@@ -2,7 +2,6 @@ package com.lendme.client;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
